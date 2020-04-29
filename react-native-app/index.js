16342b1573c902ddab0a081a6fe927e1f0412561
raymileshun <horakrajmund@windowslive.com>
2020-04-29 12:39:15 +0200

    disabled yellow info box

--- react-native-app/index.js
+++ react-native-app/index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import EntryPoint from './EntryPoint';
 import {name as appName} from './app.json';
-
+console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => EntryPoint);
